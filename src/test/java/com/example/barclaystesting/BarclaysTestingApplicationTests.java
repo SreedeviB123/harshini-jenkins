13e497d1b26b506cb662398826e921537f155dda
harshini <yuvaaishu12@gmail.com>
2021-09-21 14:37:24 +0530

    first commit

---
+++ src/test/java/com/example/barclaystesting/BarclaysTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barclaystesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarclaysTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
